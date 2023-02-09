8182de1585f0e20a59e4016f9d5621dee3c4f10c
vartuom <v.artuom@ya.ru>
2023-02-09 22:01:36 +0800

    09-02-home-1

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,9 +1,12 @@
 import { Module } from "@nestjs/common";
 import { AppController } from "./app.controller";
 import { UsersModule } from "./users/users.module";
+import { WishesModule } from './wishes/wishes.module';
+import { WishlistsModule } from './wishlists/wishlists.module';
+import { OffersModule } from './offers/offers.module';
 
 @Module({
-    imports: [UsersModule],
+    imports: [UsersModule, WishesModule, WishlistsModule, OffersModule],
     controllers: [AppController],
     providers: [],
 })
